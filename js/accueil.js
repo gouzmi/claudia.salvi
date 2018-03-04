42849b3a082c0b15cb99a2d7001b325967957a3e
gouzmi <guillaume.dupont.rm@gmail.com>
2018-03-04 23:35:49 +0100

    bcp de modif

--- js/accueil.js
+++ js/accueil.js
@@ -19,17 +19,15 @@ function header(){
     $(window).scroll(function() {
       var windowBottom = $(this).scrollTop() + $(this).innerHeight();
       $(".fade").each(function() {
-        /* Check the location of each desired element */
+
         var objectBottom = $(this).offset().top + $(this).outerHeight();
 
-        /* If the element is completely within bounds of the window, fade it in */
-        if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+
+        if (objectBottom < windowBottom) {
           if ($(this).css("opacity")==0) {
             $(this).fadeTo(100,1);}
-        } else { //object goes out of view (scrolling up)
-          //if ($(this).css("opacity")==1) {
-            //$(this).fadeTo(100,0);}
+        } else {
         }
       });
-    }).scroll(); //invoke scroll-handler on page-load
+    }).scroll();
   });
