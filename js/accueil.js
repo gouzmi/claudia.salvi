3d6ec9513005f54d456a1299ba1235b04e345d20
gouzmi <guillaume.dupont.rm@gmail.com>
2018-03-20 00:57:28 +0100

    masse

--- js/accueil.js
+++ js/accueil.js
@@ -14,24 +14,6 @@ function header(){
 }
 
 
-
-  $(window).on("load",function() {
-    $(window).scroll(function() {
-      var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-      $(".fade").each(function() {
-
-        var objectBottom = $(this).offset().top + $(this).outerHeight();
-
-
-        if (objectBottom < windowBottom) {
-          if ($(this).css("opacity")==0) {
-            $(this).fadeTo(100,1);}
-        } else {
-        }
-      });
-    }).scroll();
-  }); 
-
 function openNav() {
     document.getElementById("mySidenav").style.width = "80%";
     document.getElementById('menu').style.backgroundColor = "rgba(0,0,0,0.4)";
